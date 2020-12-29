43220ebde8d8fb5d40cce66b9b495cf9f01045a8
NatureLingRan <1127711564@qq.com>
2020-12-29 11:24:47 +0800

    格式化

--- check.sh
+++ check.sh
@@ -3,23 +3,26 @@
 
 log() {
     logfile=./log
-    msg="`date +'%F %H:%M:%S'`\t[$1]\t$2\033[0m"
+    msg="$(date +'%F %H:%M:%S')\t[$1]\t$2\033[0m"
 
-    case $1 in 
-        info)
-            echo -e "\033[32m$msg" | tee -a $logfile;;
-        warn)
-            echo -e "\033[33m$msg" | tee -a $logfile;;
-        error)
-            echo -e "\033[31m$msg" | tee -a $logfile;;
+    case $1 in
+    info)
+        echo -e "\033[32m$msg" | tee -a $logfile
+        ;;
+    warn)
+        echo -e "\033[33m$msg" | tee -a $logfile
+        ;;
+    error)
+        echo -e "\033[31m$msg" | tee -a $logfile
+        ;;
     esac
 }
 
 checkCMD() {
-    for v in $*;do   
-        if type $v  >/dev/null 2>&1; then
+    for v in $*; do
+        if type $v >/dev/null 2>&1; then
             log info "工具 $v 安装成功"
-        else 
+        else
             log error "工具 $v 未安装"
         fi
     done
--- kali/init.sh
+++ kali/init.sh
@@ -10,11 +10,10 @@ log() {
 }
 
 install() {
-for app in $*
-do
-   log 安装$app
-   sudo apt install -y $app >> log 2>&1 
-done
+    for app in $*; do
+        log 安装$app
+        sudo apt install -y $app >>log 2>&1
+    done
 }
 
 ##*************************安装常用软件*********************************
@@ -26,15 +25,15 @@ install htop fzf highlight
 log 安装oh-myzsh
 # 因为国内访问raw.githubusercontent.com不稳定所以改为手动安装
 #sh -c "$(curl -fsSL --retry 10 --connect-timeout 3 https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh >>log 2>&1 
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh >>log 2>&1
 
 cp $ROOTPATH/zsh/zshrc.conf ~/.zshrc
 
 log 安装语法高亮
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting >>log 2>&1 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting >>log 2>&1
 
 log 安装自动补全
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions >>log 2>&1 
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions >>log 2>&1
 
 log 切换shell
 chsh -s $(which zsh)
