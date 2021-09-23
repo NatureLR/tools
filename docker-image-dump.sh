1224f859f7af9d7976dcb0f69916f7003285b998
NatureLR <1127711564@qq.com>
2021-09-23 14:29:41 +0800

    增加docker转储脚本

---
+++ docker-image-dump.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# 用于转储docker镜像到私有仓库的脚本
+
+# 根据情况修改
+repo="hub.docker.com"
+
+name=`echo $1 |awk -F "/" '{print$2}'`
+
+$repotag=$repo/$name
+
+docker pull $1
+docker tag $1 $repotag
+docker push $repotag
