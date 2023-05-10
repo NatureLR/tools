f4b78fa1afe52d6f70186dae160999e40ed0a7f4
NatureLR <1127711564@qq.com>
2023-05-10 22:12:08 +0800

    添加树莓派led控制脚本

---
+++ pi-led.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+
+
+# 绿色的灯
+#echo 0    | sudo tee   /sys/class/leds/led0/brightness
+#echo none | sudo tee  /sys/class/leds/led0/trigger
+# 红色的灯
+#echo none | sudo tee  /sys/class/leds/led1/trigger
+#echo 0    | sudo tee  /sys/class/leds/led1/brightness
+
+greenLED="/sys/class/leds/led0/brightness"
+redLED="/sys/class/leds/led1/brightness"
+
+off(){
+echo 0 > $greenLED
+echo 0 > $redLED
+}
+
+on(){
+echo 1 > $greenLED
+echo 1 > $redLED
+}
+
+main(){
+    case $1 in
+    on)
+        on;;
+    off)
+        off;;
+    *)
+        echo "on 开启led off关闭led";;
+    esac
+}
+
+main "$@"
